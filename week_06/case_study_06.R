6c676f0f6ae862180ee863e6d1940414f13e9b17
Wei Liu <wliu54@buffalo.edu>
2021-10-11 21:12:25 -0400

    fisrt try!

--- week_06/case_study_06.R
+++ week_06/case_study_06.R
@@ -1,58 +1,40 @@
----
-title: "Case Study 06: Find hottest country on each continent"
-author: "Wei Liu"
-date: October 11th, 2021
-output: github_document
----
-
-## We need these following packages
-
-```{r, message=FALSE}
 library(raster)
 library(sp)
 library(spData)
 library(tidyverse)
 library(sf)
-```
 
-## Prepare the data
-
-**world ---> world_sp**
-```{r}
 data(world)  #load 'world' data from spData package
+
 world_new <- filter(world, continent!="Antarctica")
 world_sp <- as(world_new,"Spatial")
-```
 
-**tmax_monthly ---> tmax_annual**
-```{r}
 tmax_monthly <- getData(name = "worldclim", var="tmax", res=10)
-gain(tmax_monthly) <- 0.1
+
+# plot(tmax_monthly) # this will take too much time, so I don't want to do it every time I run the code.
+
+gain(tmax_monthly) <- 0.1 # I was not sure what's happening here, but the result seems correct.
+
 tmax_annual <- max(tmax_monthly)
 names(tmax_annual) <- "tmax"
-```
 
-## Extract the value and make a plot
-```{r, fig.align='center'}
+# extract the value from the raster to the polygon
 max_temp <- raster::extract(tmax_annual, world_sp,
                 fun=max, na.rm=T, small=T, sp=T) %>%
   st_as_sf()
+
+# do the plotting!!!
 ggplot(max_temp, aes(fill=tmax)) +
   geom_sf() +
   scale_fill_viridis_c(name="Annual\nMaximum\nTemperature (C)") +
   theme(legend.position = 'bottom')
-```
 
-## Find the hottest country in each continent
-```{r}
+# find the hottest country in each continent
 hottest_country <- max_temp %>%
   group_by(continent) %>%
   top_n(1, tmax) %>%
   select(name_long, continent, tmax) %>%
   st_set_geometry(NULL) %>%
   arrange(desc(tmax))
-
-hottest_country
-```
-
+View(hottest_country)
 
